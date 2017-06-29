ca31e30da74286e0aa77025439ee79599b5c826e
= <=>
2017-06-29 08:03:14 +0200

    deployed dropzone

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-    Content
+    <div class="dropzone" id="productPhoto"></div>
+@endsection
+
+@section('scripts')
+    <script>
+        {{-- var url = "{{ route('welcome') }}"; --}}
+
+        var drop = new Dropzone('#productPhoto', {
+            url: "{{ route('welcome') }}"
+        });
+    </script>
 @endsection
